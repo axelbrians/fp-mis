3ea5b4c01cbe06fc3f0c579e3f7cbe5f18c55c5f
axel briano <axelbrians@gmail.com>
2020-11-08 16:38:10 +0700

    added navigator library

---
+++ src/pages/SecondPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+class SecondPage extends React.Component{
+  render() {
+    return(
+      <View>
+        <Text>Second Page</Text>
+      </View>
+    )
+  }
+
+}
+
+export default SecondPage;
